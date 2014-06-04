cf9d6e5aed80b838b6c07143ccb9ac981a277962
Hamayama <hamay1010@gmail.com>
2014-06-04 16:27:45 +0900

    scm chg v1.01

--- msjis.scm
+++ msjis.scm
@@ -1,7 +1,7 @@
 ;; -*- coding: utf-8 -*-
 ;;
 ;; msjis.scm
-;; 2014-6-2 v1.00
+;; 2014-6-4 v1.01
 ;;
 ;; ＜内容＞
 ;;   Windows のコマンドプロンプトで Gauche(gosh.exe) を使うときに、
@@ -11,8 +11,6 @@
 ;;   (1)Gauche の開発最新版が必要です(v0.9.3.3ではエラーになります)。
 ;;   (2)コンソールの入出力についてのみ文字コードが変換されます。
 ;;      (ファイルの読み書きやリダイレクトの文字コードは変換されません)
-;;   (3)自動入力ツール等で大量の文字を一度に入力すると、エラーが
-;;      発生する場合があります。
 ;;
 ;; ＜インストール＞
 ;;   msjis.scm を Gauche でロード可能なフォルダにコピーします。
@@ -36,18 +34,25 @@
 (select-module msjis)
 
 ;; リダイレクトありかチェックする関数
-;; (Win32APIのReadConsole(),WriteConsole()は、リダイレクトありのときは使えない。
-;;  リダイレクトの有無はGetConsoleMode()が成功するかどうかで判定できる。)
+;; (リダイレクトの有無はGetConsoleMode()が成功するかどうかで判定できる)
 (define (redirected-handle? hdl)
   (guard (exc
           ((<system-error> exc) #t))
          (sys-get-console-mode hdl) #f))
 
-;; 1文字入力処理
+;; 1文字入力処理(Windows XPで行頭の文字が「g」に化けてエラーになる場合がある。原因不明)
 (define (getc-console hdl)
   (let1 buf (make-u8vector 2 0)
     (sys-read-console hdl buf)
     (string-ref (ces-convert (u8vector->string buf) 'UTF-16LE) 0)))
+
+;; 1文字入力処理2(文字化け対策版)(ただしCP932専用)
+(define (getc-console2 hdl)
+  (let1 buf (make-u8vector 2 0)
+    (read-block! buf (standard-input-port) 0 1)
+    ;; 2バイト文字のチェック(CP932専用)
+    (if (>= (u8vector-ref buf 0) #x80) (read-block! buf (standard-input-port) 1 2))
+    (string-ref (ces-convert (u8vector->string buf) 'CP932) 0)))
 
 ;; 1文字出力処理
 (define (putc-console hdl chr)
@@ -61,7 +66,9 @@
 (define (make-console-stdin-port)
   (let1 hdl (sys-get-std-handle STD_INPUT_HANDLE)
     (if (redirected-handle? hdl) #f
-      (make <virtual-input-port> :getc (cut getc-console hdl)))))
+      ;(make <virtual-input-port> :getc (cut getc-console hdl))
+      (make <virtual-input-port> :getc (cut getc-console2 hdl))
+      )))
 
 ;; 標準出力の変換ポートの作成
 (define (make-console-stdout-port)
